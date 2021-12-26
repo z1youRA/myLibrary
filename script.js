7ffed4cffc5548b0b5e2a5a0f9d99966a9ffc024
z1youRA <374671110@qq.com>
2021-12-26 19:52:55 +0800

    add sidebar headline and input columns

--- script.js
+++ script.js
@@ -40,10 +40,15 @@ function displayBook() {
         const author = document.createElement('div');
         const pages = document.createElement('div');
         const status = document.createElement('div');
+        book.classList.add('card');
         title.textContent = bookInLib.title;
+        title.classList.add('title');
         author.textContent = bookInLib.author;
+        author.classList.add('author');
         pages.textContent = bookInLib.pages;
+        pages.classList.add('pages');
         status.textContent = bookInLib.status;
+        status.classList.add('status');
         book.appendChild(title);
         book.appendChild(author);
         book.appendChild(pages);
@@ -52,7 +57,11 @@ function displayBook() {
     })
 }
 
-const book = new Book('the odin', 'me', 100, 0);
-let myLibrary = [book];
-addBookToLibrary();
+const book1 = new Book('the odin', 'me', 100, 0);
+const book2 = new Book('the odin', 'me', 100, 0);
+const book3 = new Book('the odin', 'me', 100, 0);
+const book4 = new Book('the odin', 'me', 100, 0);
+const book5 = new Book('the odin', 'me', 100, 0);
+const book6 = new Book('the odin', 'me', 100, 0);
+let myLibrary = [book1, book2, book3, book4, book5, book6];
 displayBook();
